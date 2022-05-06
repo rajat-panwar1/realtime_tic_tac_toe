b071d7f10c9b834c206c80ffbf2d924374d3c918
Rajat Panwar <hail4heisenberg@gmail.com>
2022-05-06 05:58:52 +0530

    last commit

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-import "./App.css";
-import { io } from "socket.io-client";
-import socketService from "./services/socketService";
-import { JoinRoom } from "./components/joinRoom";
-import GameContext, { IGameContextProps } from "./gameContext";
-import { Game } from "./components/game";
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import './App.css';
+import { io } from 'socket.io-client';
+import socketService from './services/socketService';
+import { JoinRoom } from './components/joinRoom';
+import GameContext, { IGameContextProps } from './gameContext';
+import { Game } from './components/game';
 
 const AppContainer = styled.div`
   width: 100%;
@@ -31,15 +31,15 @@ const MainContainer = styled.div`
 
 function App() {
   const [isInRoom, setInRoom] = useState(false);
-  const [playerSymbol, setPlayerSymbol] = useState<"x" | "o">("x");
+  const [playerSymbol, setPlayerSymbol] = useState<'x' | 'o'>('x');
   const [isPlayerTurn, setPlayerTurn] = useState(false);
   const [isGameStarted, setGameStarted] = useState(false);
 
   const connectSocket = async () => {
     const socket = await socketService
-      .connect("http://localhost:9000")
-      .catch((err) => {
-        console.log("Error: ", err);
+      .connect('https://hyzen-tik-tac.herokuapp.com/')
+      .catch(err => {
+        console.log('Error: ', err);
       });
   };
 
